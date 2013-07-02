e5c9ae76298a52691e98fd0bb37edb6d94756bc1
Zeng Yun <yun.zeng@gezbox.com>
2013-07-03 07:53:46 +0800

    Initial Commit

---
+++ OauthSetSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OauthSetSample
+//
+//  Created by dreamer on 13-7-3.
+//  Copyright (c) 2013年 ZengYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OauthSetSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OauthSetSample
+//
+//  Created by dreamer on 13-7-3.
+//  Copyright (c) 2013年 ZengYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
